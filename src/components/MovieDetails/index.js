7f0c4b2c7049dbea7e0b0d66b61de61642089493
vitcool@ukr.net <vitcool@ukr.net>
2020-03-19 07:14:42 +0200

    Task 1 - modal information amendments

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './index.css'
+
 const MovieDetails = ({ modalProps, handleModalClose }) => {
+  const {
+    title,
+    vote_average,
+    popularity,
+    overview,
+    release_date
+  } = modalProps
+
   return (
-    <>
-      <h1>{modalProps.title}</h1>
-      <button onClick={handleModalClose} type="button">
-        Close
-      </button>
-    </>
+    <div className="modal-wrapper">
+      <div className="modal-header">
+        <h4 className="modal-title">{title}</h4>
+        <button
+          onClick={handleModalClose}
+          type="button"
+          className="close-button"
+        >
+          X
+        </button>
+      </div>
+      <div className="modal-content">
+        <p className="content-item">Rating: {vote_average}</p>
+        <p className="content-item">Popularuty:{popularity}</p>
+        <p className="content-item">Overview: {overview}</p>
+        <p className="content-item">Release date: {release_date}</p>
+      </div>
+    </div>
   )
 }
 
